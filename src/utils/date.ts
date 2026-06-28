53da5fa5bfb8e1a394247e4c6aa555bbdc5c5bbb
Pallavi Tekta <pallavitekta@Pallavis-MacBook-Air.local>
2026-06-28 23:42:29 +0530

    Continue thoughts mogration

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -16,8 +16,8 @@ export function getFormattedDate(
 }
 
 export function collectionDateSort(
-	a: CollectionEntry<"post" | "note">,
-	b: CollectionEntry<"post" | "note">,
+	a: CollectionEntry<"post" | "thought">,
+	b: CollectionEntry<"post" | "thought">,
 ) {
 	return b.data.publishDate.getTime() - a.data.publishDate.getTime();
 }
